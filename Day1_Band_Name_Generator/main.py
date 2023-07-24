93a30b032a52048619cc0d2fd703e14fc0caf24f
Nasim <132076501+Nasim-RN@users.noreply.github.com>
2023-07-24 18:03:37 +0200

    Add files via upload

---
+++ Day1_Band_Name_Generator/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in? \n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
