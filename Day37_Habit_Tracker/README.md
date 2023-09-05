e29006c7f8dc2c56119f3bb3257a53d55c9710d2
Nasim <132076501+Nasim-RN@users.noreply.github.com>
2023-09-05 16:42:18 +0200

    Add files via upload

---
+++ Day37_Habit_Tracker/README.md
@@ -0,0 +1,8 @@
+# Day 37 - Habit Tracking Project: API Post Requests & Headers
+## Concepts Practised
+- HTTP Post Requests
+- Advanced Authentication using an HTTP Header
+- Autofilling dates using strftime function
+- How to use HTTP Put and Delete Requests
+## Habit Tracking
+![Screenshot 2023-09-05 at 16.33.03.png](..%2F..%2F..%2FDesktop%2FScreenshot%202023-09-05%20at%2016.33.03.png)
---
+++ Day37_Habit_Tracker/main.py
@@ -0,0 +1,64 @@
+import requests
+import os
+from datetime import datetime
+
+TOKEN = os.environ.get('TOKEN')
+USERNAME = os.environ.get('USERNAME')
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "id": GRAPH_ID,
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":"graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint,json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(year=2023, month=8, day=17)
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "9.5"
+
+        # input("How many kilometers did you cycle today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5",
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
