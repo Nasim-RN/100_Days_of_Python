9c769b818d18c158fbe0c6a3e577754cbc690f65
Nasim <132076501+Nasim-RN@users.noreply.github.com>
2023-09-05 13:50:21 +0200

    Update main.py

--- Day36_Trading_News_Alert/main.py
+++ Day36_Trading_News_Alert/main.py
@@ -1,53 +1,59 @@
 import requests
+import os
 from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-STOCK_API_KEY = "YOUR OWN API KEY FROM ALPHAVANTAGE"
-NEWS_API_KEY = "YOUR OWN API KEY FROM NEWSAPI"
-TWILIO_SID = "YOUR TWILIO ACCOUNT SID"
-TWILIO_AUTH_TOKEN = "YOUR TWILIO AUTH TOKEN"
+STOCK_API_KEY = os.environ.get('ALPHAVANTAGE')
+NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+TWILIO_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 
-VIRTUAL_TWILIO_NUMBER = "your virtual twilio number"
-VERIFIED_NUMBER = "your own phone number verified with Twilio"
+VIRTUAL_NUMBER = os.environ.get('TWILIO_VIRTUAL_NUMBER')
+VERIFIED_NUMBER = os.environ.get('VERIFIED_REAL_NUMBER')
 
+
+# Get yesterday's closing stock price
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
     "apikey": STOCK_API_KEY,
 }
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-data = response.json()["Time Series (Daily)"]
+data = response.json()['Time Series (Daily)']
+print(data)
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = float(yesterday_data["4. close"])
 print(yesterday_closing_price)
 
-#Get the day before yesterday's closing stock price
+
+# Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = float(day_before_yesterday_data["4. close"])
 print(day_before_yesterday_closing_price)
 
-#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 difference = yesterday_closing_price - day_before_yesterday_closing_price
 print(difference)
-
 up_down = None
 if difference > 0:
     up_down = "🔺"
 else:
     up_down = "🔻"
 
-#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+# Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday
 diff_percent = round((difference / yesterday_closing_price) * 100)
 print(diff_percent)
 
-#If difference percentage is greater than 5 then use the News API to get the first 3 news pieces for the COMPANY_NAME.
-if abs(diff_percent) >= 5 :
+# If difference percentage is greater than 5 then use the News API to get the first 3 news pieces for the COMPANY_NAME.
+if abs(diff_percent) >= 5:
     news_params = {
         "apikey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -56,25 +62,23 @@ if abs(diff_percent) >= 5 :
     news_responce = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_responce.json()["articles"]
     print(articles)
-# Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# Use Python slice operator to create a list that contains the first 3 articles.
     three_articles = articles[:3]
     print(three_articles)
 
 
-#Create a new list of the first 3 article's headline and description using list comprehension.
-    # formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}.\nBrief: {article['description']}"]
-
-    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}\nBrief: {article['description']}" for article in three_articles]
+# Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [(f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}\n"
+                           f"Brief: {article['description']}") for article in three_articles]
     print(formatted_articles)
 
-#Send each article as a separate message via Twilio.
+# Send each article as a separate message via Twilio.
     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
     for article in formatted_articles:
         message = client.messages.create(
             body=article,
-            from_=VIRTUAL_TWILIO_NUMBER,
+            from_=VIRTUAL_NUMBER,
             to=VERIFIED_NUMBER,
         )
         print(message.sid)
         print(message.status)
-
