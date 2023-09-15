92bd9edcfee5900fdd0e45d26029782718cbd874
Nasim <132076501+Nasim-RN@users.noreply.github.com>
2023-09-15 23:54:16 +0200

    Add files via upload

---
+++ Day39_Cheap_Flight_Finder/data_manager.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+SHEETY_PRICES_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
---
+++ Day39_Cheap_Flight_Finder/notification_manager.py
@@ -0,0 +1,22 @@
+from twilio.rest import Client
+import os
+
+TWILIO_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_VIRTUAL_NUMBER = os.environ.get('TWILIO_VIRTUAL_NUMBER')
+TWILIO_VERIFIED_NUMBER = os.environ.get('VERIFIED_REAL_NUMBER')
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
